abb3e82f3c4d1d4b28205622cab1d95a94830b6e
iceweasel <johnsjosephk@protonmail.com>
2021-04-27 00:45:27 +0530

    initial layout done Dynamic posts working

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,18 +5,18 @@
     <div class="row">
       
        <div class="col-3 pe-5">
-        <img src="/svg/nat.jpg"   style="border-radius:50%" >
+        <img src="/svg/gop.jpeg"  style="width:200px;border-radius:50%;" >
        </div>
        
        <div class="col-9 pt-5" >
         
         <div class="d-flex justify-content-between align-items-baseline ">
          <div><h1>{{ $user-> username }}</h1></div>
-         <a href="#">Add new post</a>
+         <a href="/p/create">Add new post</a>
         </div>
 
             <div class="d-flex">
-               <div class="pr-3"><strong>900</strong> Posts</div>
+               <div class="pr-3"><strong>{{ $user->posts->count()}}</strong> Posts</div>
                <div class="pr-3"><strong>900</strong> followers</div>
                <div class="pr-3"><strong>900</strong> following</div>
             </div>
@@ -29,13 +29,15 @@
     </div>
 
     <div class="row pt-4">
-      <div class="col-4"><img style="height:300px;" src="https://static.tvtropes.org/pmwiki/pub/images/blackwidow_endgameprofile.jpg" class="w-100"></div>
-  
-      <div class="col-4"><img style="height:300px;" src="https://static.tvtropes.org/pmwiki/pub/images/blackwidow_endgameprofile.jpg" class="w-100"></div>
 
-      <div class="col-4"><img style="height:300px;" src="https://static.tvtropes.org/pmwiki/pub/images/blackwidow_endgameprofile.jpg" class="w-100"></div>
-
-
+    @foreach($user->posts as $post)
+            <div class="col-4 pb-4">
+                <a href="/p/{{ $post->id }}">
+                    <img src="/storage/{{ $post->image }}"  style="height:450px;" class="w-100">
+                </a>
+            </div>
+        @endforeach
+      
     </div>
 
 
@@ -45,6 +47,17 @@
 @endsection
 <?php
 //   {{ $user->profile->title  }}
+/*
 
+<div class="col-4"><img style="height:300px;" src="https://static.tvtropes.org/pmwiki/pub/images/blackwidow_endgameprofile.jpg" class="w-100"></div>
+  
+      <div class="col-4"><img style="height:300px;" src="https://static.tvtropes.org/pmwiki/pub/images/blackwidow_endgameprofile.jpg" class="w-100"></div>
+
+      <div class="col-4"><img style="height:300px;" src="https://static.tvtropes.org/pmwiki/pub/images/blackwidow_endgameprofile.jpg" class="w-100"></div>
+
+
+
+*/
 
 ?>
+
